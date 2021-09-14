d56d424cf21c7ddf5b2c49033e797de2ca872950
Wladek <vladmasliuk@gmail.com>
2021-09-14 17:30:48 +0200

    create navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link'
+
+// assets
+import Logo from '../assets/logo.png'
 const Navbar = () => {
     return(
-        <div>
-            <h1>Navbar</h1>
+        <div className="header">
+            <div className="container">
+                <div className="headerContent">
+                    <Link href="/">
+                        <a>
+                            <Image src={Logo} alt="Social media" width={70} height={70}/>
+                        </a>
+                    </Link>
+                    <ul>
+                        <li className="menu-item"><Link href="/"><a>Home</a></Link></li>
+                        <li className="menu-item"><Link href="/"><a>About</a></Link></li>
+                        <li className="menu-item"><Link href="/"><a>Download</a></Link></li>
+                        <li className="menu-item"><Link href="/"><a>Contact</a></Link></li>
+                        <li className="sing-link"><Link href="https://react-social-a3a4f.web.app/"><a target="_blank" >Sign in</a></Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      
     </div>
   )
 }
