f8fd1173c6ce8441ff0dd370bc399e660cd60fad
Wladek <vladmasliuk@gmail.com>
2021-09-15 19:06:43 +0200

    create about section

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,45 @@
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link'
 
 // assets
-import Logo from '../assets/img/logo.png'
-const Navbar = () => {
-    return(
-        <div className="header">
-            <div className="container">
-                <div className="headerContent">
-                    <Link href="/">
-                        <a>
-                            <Image src={Logo} alt="Social media" width={70} height={70}/>
-                        </a>
-                    </Link>
-                    <ul>
-                        <li className="menu-item"><Link href="/"><a>Home</a></Link></li>
-                        <li className="menu-item"><Link href="/"><a>About</a></Link></li>
-                        <li className="menu-item"><Link href="/"><a>Download</a></Link></li>
-                        <li className="menu-item"><Link href="/"><a>Contact</a></Link></li>
-                        <li className="sing-link"><Link href="https://react-social-a3a4f.web.app/"><a target="_blank" >Sign in</a></Link></li>
-                    </ul>
+import Logo from '../assets/img/logo.png';
+
+class Navbar extends React.Component{
+    state={
+        isTop: false,
+      }
+    
+    componentDidMount() {
+        document.addEventListener('scroll', () => {
+            const isTop = window.scrollY > 100;
+            if (isTop !== this.state.isTop) {
+            this.setState({ isTop })
+            }
+        });
+    }
+    render(){
+        return(
+            <div className={`header ${this.state.isTop ? 'header-scroll' : null}`}>
+                <div className="container">
+                    <div className="headerContent">
+                        <Link href="/">
+                            <a>
+                                <Image src={Logo} alt="Social media" width={70} height={70}/>
+                            </a>
+                        </Link>
+                        <ul>
+                            <li className="menu-item"><Link href="/"><a>Home</a></Link></li>
+                            <li className="menu-item"><Link href="/"><a>About</a></Link></li>
+                            <li className="menu-item"><Link href="/"><a>Download</a></Link></li>
+                            <li className="menu-item"><Link href="/"><a>Contact</a></Link></li>
+                            <li className="sing-link"><Link href="https://react-social-a3a4f.web.app/"><a target="_blank" >Sign in</a></Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
 
-export default function Home() {
-  return (
-    <div>
-      {/* video sec */}
-      <div className="video-sec">
-        {/* video */}
-        <video autoPlay muted loop>
-            <source src="../video/video-bg.mp4" type="video/mp4"/>
-        </video>
-        {/* text */}
-        <div className="video-sec-text">
-          <h1>Social media</h1>
-          <p>explore the perfect communication</p>
+// assets
+import AboutImg from '../assets/img/about-img.jpg';
+
+class Home extends React.Component{
+  render() {
+    return (
+      <div>
+        {/* video sec */}
+        <div className="video-sec">
+          {/* video */}
+          <video autoPlay muted loop>
+              <source src="../video/video-bg.mp4" type="video/mp4"/>
+          </video>
+          {/* text */}
+          <div className="video-sec-text">
+            <h1>Social media</h1>
+            <p>explore the perfect communication</p>
+          </div>
+          {/* scroll mouse */}
+          <div className="scroll-downs">
+            <div className="mousey">
+              <div className="scroller"></div>
+            </div>
+          </div>
         </div>
-        {/* scroll mouse */}
-        <div className="scroll-downs">
-          <div className="mousey">
-            <div className="scroller"></div>
+        {/* about sec */}
+        <div className="container">
+          <div className="about-sec">
+            <div className="about-sec-text">
+              <h2>About</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+                in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div>
+              <Image src={AboutImg} alt="Social media screen" width={750} height={450}/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default Home;
